2039899f3fd7c96b5cafb46405556561564dedfd
Matheus Soares <m.souza19@unifesp.br>
2024-10-04 17:17:06 -0300

    Nome completo com método secundário

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,6 +1,19 @@
-public class MinhaClasse
-{
+public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print("olá turma");
+
+        /* metodo principal = main */
+
+        String primeiroNome = "Matheus";
+        String segundoNome = "Soares";
+        String nomeInteiro = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeInteiro);
     }
+
+    /* criando outro metodo para a classe */
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método :" + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+
 }
+
